8b1d8105a68bad41430c5a582deeb8aa3df45873
unknown <Steven@DESKTOP-24FE1DO.13437>
2020-04-22 16:18:26 +0100

    Added team registration and auth

--- bot.js
+++ bot.js
@@ -3,6 +3,34 @@ const client = new Discord.Client();
 
 const token = "";
 
+var competition = {"teams": [], "judges": []};
+
+function isAuthorised(user, level, exclusive) {
+	if (exclusive) {
+		const highestRole = user.roles.highest;
+		if (highestRole.name === level) {
+			return true;
+		}
+	} else {
+		getRoleByName(user.guild.roles, level).then(role => {
+			if (highestRole.comparePositionTo(role.first()) >= 0) {
+				return true;
+			}
+		});
+	}
+	return false;
+}
+
+function doesUserHaveRole(user, roleName) {
+	return getRoleByName(msg.guild.roles, roleName).then(roleID => {
+			if (user.roles.cache.has(roleID.firstKey())) {
+				return true;
+			} else {
+				return false;
+			}
+	});
+}
+
 function getRoleByName(rm, name) {
 	return rm.fetch().then(roles => roles.cache.filter( role => role.name === name ));
 }
@@ -22,24 +50,60 @@ function helpCommand(msg) {
 				name: "Tab Commands", value: "None"
 			},
 			{
-				name: "Speaker Commands", value: "**!register** <Your Name> - register yourself as a Speaker"
+				name: "Speaker Commands", value: "**!register** <Your Name> <Speaker/Judge> - register yourself as a Speaker"
 			},
 			{
-				name: "Team Commands", value: "None"
+				name: "Team Commands", value: "**!team** <@Teammate> <Team Name> - register a team with your teammate."
 			}
 		);
 	
 	msg.reply(embedMessage);
 }
 
-function registerSpeaker(msg, name) {
+function registerUser(msg, name, type) {
 	const fullname = name.join(" ");
 	const targetGM = msg.member;
-	getRoleByName(msg.guild.roles, "Speaker").then(speakerRole => {
-		targetGM.setNickname(fullname).catch(console.error);
-		targetGM.roles.add(speakerRole).catch(console.error);
-		msg.reply(`Registered ${fullname}`);
-	});
+	const roleName = (type.toLowerCase() == "speaker" ? "Speaker" : "Judge");
+	if (isAuthorised(targetGM, "@everyone", true)) {
+		getRoleByName(msg.guild.roles, roleName).then(speakerRole => {
+			targetGM.setNickname(fullname).catch(console.error);
+			targetGM.roles.add(speakerRole).catch(console.error);
+			msg.reply(`Registered ${fullname} as a ${roleName}.`);
+		});
+	} else {
+		msg.reply("You have already registered!");
+	}
+}
+
+function storeTeam(speakerOne, speakerTwo, teamName) {
+	// competition.teams.push({name: teamName, speakers: [speakerOne.id, speakerTwo.id]});
+}
+
+function registerTeam(msg, name) {
+	const teamname = name.join(" ");
+	const speakerOne = msg.member;
+	const speakerTwo = msg.mentions.members.first();
+	if (typeof(speakerTwo) === typeof(undefined)) {
+		msg.reply("Invalid teammate");
+	} else {
+		getRoleByName(msg.guild.roles, "Speaker").then(speakerRole => {
+			if (speakerOne.roles.cache.has(speakerRole.firstKey()) && speakerTwo.roles.cache.has(speakerRole.firstKey())) {
+				getRoleByName(msg.guild.roles, "On Team").then(teamRole => {
+					if (speakerOne.roles.cache.has(teamRole.firstKey()) || speakerTwo.roles.cache.has(teamRole.firstKey())) {
+						msg.reply("You and your team mate cannot already be on a team.");
+					} else {
+						storeTeam(speakerOne, speakerTwo, teamname);
+						speakerOne.setNickname(`[${teamname}] ${speakerOne.nickname.split("] ").pop()}`.substring(0,32));
+						speakerTwo.setNickname(`[${teamname}] ${speakerTwo.nickname.split("] ").pop()}`.substring(0,32));
+						speakerOne.roles.add(teamRole).catch(console.error);
+						speakerTwo.roles.add(teamRole).catch(console.error);
+					}
+				});
+			} else {
+				msg.reply("Both speakers must register as a speaker using !register.");
+			}
+		});
+	}
 }
 
 client.on('ready', () => {
@@ -56,9 +120,21 @@ client.on('message', msg => {
 			case "!register":
 				if (command.length == 1) {
 					msg.reply("You need to supply a name")
+				} else if (command[command.length - 1].toLowerCase() !== "speaker" && command[command.length - 1].toLowerCase() !== "judge") {
+					msg.reply("You must specify if you are a Judge or a Speaker!");
 				} else {
-					registerSpeaker(msg, command.slice(1));
+					registerUser(msg, command.slice(1, command.length - 1), command[command.length - 1]);
 				}
+				break;
+			case "!team":
+				if (command.length <= 2) {
+					msg.reply("You need to supply a team name");
+				} else if (msg.mentions.members === undefined) {
+					msg.reply("You must mention your teammate by placing the @ symbol before their name");
+				} else {
+					registerTeam(msg, command.slice(2));
+				}
+				break;
 		}
 	}
 });
