76a871dafc07565b19026fd3d0d073d27a22ac18
unknown <Steven@DESKTOP-24FE1DO.26063>
2020-04-28 18:34:48 +0100

    g

--- csv-processor.py
+++ csv-processor.py
@@ -9,23 +9,39 @@ csvjudges = "judges.csv"
 
 teams = []
 judges = []
+speakers = []
+safeguardS = []
+safeguardJ = []
 
-with open(csvteams) as csvfile:
+with open(csvteams, encoding="utf8") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     next(reader)
     for row in reader:
         teamname = row[0]
         sOne = row[1]
         sTwo = row[2]
+        safeguard = row[3]
+        if safeguard.lower().replace(" ", "") == "child":
+            safeguard = True
+        else:
+            safeguard = False
         teams.append(teamname.lower())
+        speakers.append(sOne.lower())
+        safeguardS.append(safeguard)
+        speakers.append(sTwo.lower())
+        safeguardS.append(safeguard)
 
-with open(csvjudges) as csvfile:
+with open(csvjudges, encoding="utf8") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     next(reader) # skip headers
     for row in reader:
         judges.append(row[0].lower())
+        if row[1].lower().replace(" ", "") == "child":
+            safeguardJ.append(True)
+        else:
+            safeguardJ.append(False)
 
-regdata = {"teams": teams, "judges": judges}
+regdata = {"teams": teams, "judges": judges, "speakers": speakers, "childrenS": safeguardS, "childrenJ": safeguardJ}
 
 f = open("regdata.json", "w")
 f.write(json.dumps(regdata))
