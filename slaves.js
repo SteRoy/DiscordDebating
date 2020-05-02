92c00e6d4f67ec4d509ecee4238c46ae3098a699
unknown <Steven@DESKTOP-24FE1DO.26063>
2020-05-02 15:50:21 +0100

    oh lord bug fixes

--- slaves.js
+++ slaves.js
@@ -13,31 +13,57 @@ class SlaveBot {
 	}
 	
 	allocateUserToRoom(guild, userID, channelName) {
-		const newChannel = getChannelByName(guild, channelName);
+		const newChannel = guild.channels.cache.find(channel => channel.name === channelName);
 		if (typeof(newChannel) === typeof(undefined)) {
 			console.log(`Invalid assignment channel specified for ${userID} - ${channelName}`);
 		} else {
 			this.client.users.fetch(userID).then(slaveUser => {
 				// May not be online
 				if (typeof(slaveUser) !== undefined) {
-					if (typeof (slaveUser.presence) !== undefined && slaveUser.presence.member.voice.channel !== null) {
-						slaveUser.presence.member.voice.channel.setChannel(newChannel);
+					if (typeof(slaveUser.presence) !== undefined && slaveUser.presence.member.voice.channel !== null) {
+						slaveUser.presence.member.voice.setChannel(newChannel);
 					} else {
-						console.log(`${user.nickname} is not in a voice channel - ${newChannel.name}`);
+						console.log(`${slaveUser.displayName} is not in a voice channel - ${newChannel.name}`);
 					}
 				} else {
-					console.log(`Could not assign ${user.name}!`)
+					console.log(`Could not assign ${slaveUser.displayName}!`)
 				}
-			});
+			}).catch(err => {});
 		}
+	}
+	
+	updateComp(comp) {
+		this.competition = comp;
 	}
 	
 	handleRoom(room, guild) {
 		for (let i = 1; i < 4; i++) {
-			const team = this.competition.teams.find(t => t.name.toLowerCase() === room.teams[i].toLowerCase());
-			team.speakers.forEach(sID => {
-				this.allocateUserToRoom(guild, sID, `${room.venue} - Debate Room`);
-			});
+			if (typeof(this.competition) === typeof(undefined)) {
+				console.log("ERRORED");
+			} else {
+				try {
+					const team = this.competition.teams.find(t => t.name.toLowerCase() === room.teams[i].toLowerCase());
+					team.speakers.forEach(sID => {
+						allocateUserToRoom(guild, sID, `${room.venue} - Debate Room`);
+					});
+				} catch(err) {
+				}
+			}
+		}
+	}
+	
+	handlePrep(room, guild) {
+		const positions = ["OG", "OO", "CG", "CO"];
+		if (typeof(this.competition) === typeof(undefined)) {
+			console.log(`Errored in room ${room.venue}`);
+		} else {
+			for (let i = 0; i < 4; i++) {
+				const team = this.competition.teams.find(t => t.name.toLowerCase() === room.teams[i].toLowerCase());
+				team.speakers.forEach(sID => {
+					const roomname = i === 0 ? `${room.venue} - Debate Room` : `${positions[i]} - Prep Room [${room.venue}]`;
+					this.allocateUserToRoom(guild, sID, roomname);
+				});
+			}
 		}
 	}
 }
