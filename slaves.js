ce1d35fa5885b3eabb6b07e0b08587f39c05321b
frijjoasis <45109609+frijjoasis@users.noreply.github.com>
2020-05-02 08:24:20 +0100

    null checks

--- slaves.js
+++ slaves.js
@@ -18,10 +18,15 @@ class SlaveBot {
 			console.log(`Invalid assignment channel specified for ${userID} - ${channelName}`);
 		} else {
 			this.client.users.fetch(userID).then(slaveUser => {
-				if (slaveUser.presence.member.voice.channel !== null){
-					slaveUser.presence.member.voice.channel.setChannel(newChannel);
+				// May not be online
+				if (typeof(slaveUser) !== undefined) {
+					if (typeof (slaveUser.presence) !== undefined && slaveUser.presence.member.voice.channel !== null) {
+						slaveUser.presence.member.voice.channel.setChannel(newChannel);
+					} else {
+						console.log(`${user.nickname} is not in a voice channel - ${newChannel.name}`);
+					}
 				} else {
-					console.log(`${user.nickname} is not in a voice channel - ${newChannel.name}`);
+					console.log(`Could not assign ${user.name}!`)
 				}
 			});
 		}
